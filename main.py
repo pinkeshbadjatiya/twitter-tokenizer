32fa423e5eacffe030bdb571ff44ed944301f7bc
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2016-08-10 23:36:26 +0530

    Create Flask app + Cleanup

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
 import re
-from parse_tweet import Tweet
+from parse_tweet import Tweet, SUBSTITUTION_STARTING_CHAR, SUBSTITUTION_ENDING_CHAR
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
 class Tokeniser:
-
     def __init__(self, file_name):
         self.file_name = file_name
         self.regex_tweet_check = re.compile(r"^RT @[\w\d]+:")
@@ -26,17 +27,49 @@ class Tokeniser:
         self.tweets.append(Tweet(tw))
 
 
-
 def write_to_file(file_name, tweets):
     with open(file_name, mode="w", encoding="utf8") as f:
         for tweet in tweets:
             print(tweet.tweet, file=f)
 
 
+@app.route("/")
+def main():
+    filename = "tweets.en.txt"
+    t = Tokeniser(filename)
+    t.load_tweets()
+
+    tweets = []
+    for t in t.tweets:
+        tweet = {}
+        tweet["tweet"] = t.origTweet
+        tweet["glyphs"] = []
+        tweet["unicodes"] = []
+        tweet["handles"] = []
+        tweet["hashtags"] = []
+        tweet["urls"] = []
+
+        for (k, v) in t.ReplacementDict.items():
+            if re.match(r">!GLY\d!<", k):
+                tweet["glyphs"].append(v)
+            elif re.match(r">!UNIC\d!<", k):
+                tweet["unicodes"].append(v)
+            elif re.match(r">!HAND\d!<", k):
+                tweet["handles"].append(v)
+            elif re.match(r">!HTAG\d!<", k):
+                tweet["hashtags"].append(v)
+            elif re.match(r">!URL\d!<", k):
+                tweet["urls"].append(v)
+        tweets.append(tweet)
+
+    # write_to_file("output_file.txt", t.tweets)
+    return render_template('tweets.html', filename=filename, tweets=tweets)
+
+
+@app.route("/write")
+def write():
+    write_to_file("output_file.txt", t.tweets)
+    return "Success!"
 
 if __name__ == "__main__":
-    # t = Tokeniser("tweets.en.txt")
-    t = Tokeniser("test.txt")
-    t.load_tweets()
-    write_to_file("output_file.txt", t.tweets)
-    # import pdb; pdb.set_trace()
+    app.run()
--- parse_tweet.py
+++ parse_tweet.py
@@ -8,24 +8,18 @@ SUBSTITUTION_STARTING_CHAR = ">!"
 SUBSTITUTION_ENDING_CHAR = "!<"
 
 
-
 class Tweet(object):
-    #  "ALNUM": r"(@[a-zA-Z0-9_]+)",
-    #  "UNICODE_GLYPHS": [^\u0000-\u007F]+
-    #   "URL": r"([https://|http://]?[a-zA-Z\d\/]+[\.]+[a-zA-Z\d\/\.]+)",
 
     regexp = {"RT": "^RT",
               "MT": r"^MT",
               "HASHTAG": r"(#[\w\d]+)",
-              "MENTIONS": r"(@[\w\d]+)",
               "EMAILS": r"([\w\d]+[.|\w\d])+@([\w\d]+[.])+\w+",
               "UNICODE_GLYPHS": r"[\U0001F600-\U0001F64F]+",
+              "UNICODE_CHARS": r"[\U000000A0-\U000FFFFF]+",
               "URL": r"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)",
               "SPACES": r"\s+",
               "TWITTER_HANDLE": r"(@[a-zA-Z0-9_]+)"}
 
-    __count = defaultdict(int)         # All count must be zero initially
-    ReplacementDict = {}
     unicode_glyphs_compiled = re.compile(regexp["UNICODE_GLYPHS"], re.UNICODE)
     regexp = dict((key, re.compile(value)) for key, value in regexp.items())
     regexp["UNICODE_GLYPHS"] = unicode_glyphs_compiled  # Get the unicode compiled regex
@@ -38,19 +32,20 @@ class Tweet(object):
             Hashtags - list of tags
         """
         self.origTweet, self.tweet = tweet, tweet
+        self.__count = defaultdict(int)         # All count must be zero initially
+        self.ReplacementDict = {}
 
         # All these methods update the self.tweet to appropriate replacement
-        self.tweet, self.UserHandles = Tweet.getUserHandles(self.tweet, self.regexp["TWITTER_HANDLE"])
-        self.RT = Tweet.getAttributeRT(tweet)
-        self.MT = Tweet.getAttributeMT(tweet)
+        self.RT = self.getAttributeRT(tweet)
+        self.MT = self.getAttributeMT(tweet)
         self.Owner = "UNKNOWN"
-        self.Emoticon = Tweet.getAttributeEmoticon(tweet)
+        self.Emoticon = self.getAttributeEmoticon(tweet)
 
-        self.tweet, self.Mentions = Tweet.getMentions(self.tweet, self.regexp["MENTIONS"])
-        self.tweet, self.Hashtags = Tweet.getHashtags(self.tweet, self.regexp["HASHTAG"])
-        self.tweet, self.URLs = Tweet.getURLs(self.tweet, self.regexp["URL"])
-        self.tweet, self.UnicodeGlyphs = Tweet.getAttributeGlyphs(self.tweet, self.regexp["UNICODE_GLYPHS"])
-        # self.tweet = Tweet.getAttributeGlyphs(tweet, Tweet.regexp["UNICODE_GLYPHS"])
+        self.tweet, self.UserHandles = self.getUserHandles(self.tweet, self.regexp["TWITTER_HANDLE"])
+        self.tweet, self.Hashtags = self.getHashtags(self.tweet, self.regexp["HASHTAG"])
+        self.tweet, self.URLs = self.getURLs(self.tweet, self.regexp["URL"])
+        self.tweet, self.UnicodeGlyphs = self.getAttributeGlyphs(self.tweet, self.regexp["UNICODE_GLYPHS"])
+        self.tweet, self.UnicodeChars = self.getAttributeUnicodeChars(self.tweet, self.regexp["UNICODE_CHARS"])
 
         # additional intelligence
         # if ( self.RT and len(self.UserHandles) > 0 ):  # change the owner of tweet?
@@ -62,18 +57,15 @@ class Tweet(object):
         return "owner %s, urls: %d, hashtags %d, user_handles %d, len_tweet %d, RT = %s, MT = %s" % \
                (self.Owner, len(self.URLs), len(self.Hashtags), len(self.UserHandles), len(self.tweet), self.RT, self.MT)
 
-    @staticmethod
-    def getAttributeRT(tweet):
+    def getAttributeRT(self, tweet):
         """ see if tweet is a RT """
         return re.search(Tweet.regexp["RT"], tweet.strip()) is not None
 
-    @staticmethod
-    def getAttributeMT(tweet):
+    def getAttributeMT(self, tweet):
         """ see if tweet is a MT """
         return re.search(Tweet.regexp["MT"], tweet.strip()) is not None
 
-    @staticmethod
-    def getAttributeEmoticon(tweet):
+    def getAttributeEmoticon(self, tweet):
         """ see if tweet is contains any emoticons, +ve, -ve or neutral """
         emoji = list()
         for tok in re.split(Tweet.regexp["SPACES"], tweet.strip()):
@@ -84,86 +76,77 @@ class Tweet(object):
                 emoji.append(tok)
         return emoji
 
-    @classmethod
-    def getAttributeGlyphs(cls, tweet, UNICODE_GLYPHS_REGEX):
+    def getAttributeGlyphs(self, tweet, UNICODE_GLYPHS_REGEX):
         """ see if tweet is contains any glyphs then get it! """
 
         replace_pattern = "GLY"
         replacements = []
         def replace_attr(matchobj):
             nonlocal replace_pattern, replacements
-            cls.__count[replace_pattern] += 1
-            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(cls.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
-            cls.ReplacementDict[substitution] = matchobj.group(0)
+            self.__count[replace_pattern] += 1
+            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(self.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
+            self.ReplacementDict[substitution] = matchobj.group(0)
             replacements.append(matchobj.group(0))
             return substitution
 
         return re.sub(UNICODE_GLYPHS_REGEX, replace_attr, tweet), replacements
 
-    @classmethod
-    def getUserHandles(cls, tweet, TWITTER_HANDLE_regex):
+    def getAttributeUnicodeChars(self, tweet, UNICODE_CHARS_REGEX):
+        """ see if tweet is contains any glyphs then get it! """
+
+        replace_pattern = "UNIC"
+        replacements = []
+        def replace_attr(matchobj):
+            nonlocal replace_pattern, replacements
+            self.__count[replace_pattern] += 1
+            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(self.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
+            self.ReplacementDict[substitution] = matchobj.group(0)
+            replacements.append(matchobj.group(0))
+            return substitution
+
+        return re.sub(UNICODE_CHARS_REGEX, replace_attr, tweet), replacements
+
+    def getUserHandles(self, tweet, TWITTER_HANDLE_regex):
         """ given a tweet we try and extract all user handles in order of occurrence"""
 
         replace_pattern = "HAND"
         replacements = []
         def replace_attr(matchobj):
             nonlocal replace_pattern, replacements
-            cls.__count[replace_pattern] += 1
-            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(cls.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
-            cls.ReplacementDict[substitution] = matchobj.group(0)
+            self.__count[replace_pattern] += 1
+            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(self.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
+            self.ReplacementDict[substitution] = matchobj.group(0)
             replacements.append(matchobj.group(0))
             return substitution
 
         return re.sub(TWITTER_HANDLE_regex, replace_attr, tweet), replacements
-        # return re.findall(Tweet.regexp["TWITTER_HANDLE"], tweet)
 
-    @classmethod
-    def getHashtags(cls, tweet, HASHTAG_regex):
+    def getHashtags(self, tweet, HASHTAG_regex):
         """ return all hashtags"""
 
         replace_pattern = "HTAG"
         replacements = []
         def replace_attr(matchobj):
             nonlocal replace_pattern, replacements
-            cls.__count[replace_pattern] += 1
-            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(cls.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
-            cls.ReplacementDict[substitution] = matchobj.group(0)
+            self.__count[replace_pattern] += 1
+            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(self.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
+            self.ReplacementDict[substitution] = matchobj.group(0)
             replacements.append(matchobj.group(0))
             return substitution
 
         return re.sub(HASHTAG_regex, replace_attr, tweet), replacements
-        # return re.findall(Tweet.regexp["HASHTAG"], tweet)
 
-    @classmethod
-    def getMentions(cls, tweet, MENTIONS_regex):
-        """ return all mentions"""
-
-        replace_pattern = "MEN"
-        replacements = []
-        def replace_attr(matchobj):
-            nonlocal replace_pattern, replacements
-            cls.__count[replace_pattern] += 1
-            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(cls.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
-            cls.ReplacementDict[substitution] = matchobj.group(0)
-            replacements.append(matchobj.group(0))
-            return substitution
-
-        return re.sub(MENTIONS_regex, replace_attr, tweet), replacements
-        # return re.findall(Tweet.regexp["MENTIONS"], tweet)
-
-    @classmethod
-    def getURLs(cls, tweet, URL_regex):
+    def getURLs(self, tweet, URL_regex):
         """ URL : [http://]?[\w\.?/]+"""
 
         replace_pattern = "URL"
         replacements = []
         def replace_attr(matchobj):
             nonlocal replace_pattern, replacements
-            cls.__count[replace_pattern] += 1
-            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(cls.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
-            cls.ReplacementDict[substitution] = matchobj.group(0)
+            self.__count[replace_pattern] += 1
+            substitution = SUBSTITUTION_STARTING_CHAR + replace_pattern + str(self.__count[replace_pattern]) + SUBSTITUTION_ENDING_CHAR
+            self.ReplacementDict[substitution] = matchobj.group(0)
             replacements.append(matchobj.group(0))
             return substitution
 
         return re.sub(URL_regex, replace_attr, tweet), replacements
-        # return re.findall(Tweet.regexp["URL"], tweet)
